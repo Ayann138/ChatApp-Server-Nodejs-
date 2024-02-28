38a29224053137aaaf9312baf48ba165c206f4da
Ayan Shahid <ayanshahid159@gmail.com>
2024-02-28 19:36:54 +0500

    Creating UserChat request added

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(express.json())
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 // Test the connection
 
 
@@ -29,31 +29,31 @@ const upload = multer({ storage: storage });
 
 app.post('/RegisterUser', upload.single('profilePic'), async (req, res) => {
   try {
-      const { uid, email, password, fullName, phoneNo } = req.body;
-      const profilepic = req.file; 
+    const { uid, email, password, fullName, phoneNo } = req.body;
+    const profilepic = req.file;
 
-      // Hashing the password
-      const hashedPassword = await bcrypt.hash(password, 10);
+    // Hashing the password
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-      const query = `
+    const query = `
           INSERT INTO Users (uid,email, pass, fullname, phoneNo, profilepic) 
           VALUES ($1, $2, $3, $4, $5, $6)
       `;
-      const result = await pool.query(query, [uid, email, hashedPassword, fullName, phoneNo, profilepic.filename]);
-      console.log("User Created with UID: " , uid)
-      res.status(200).send('User created successfully');
+    const result = await pool.query(query, [uid, email, hashedPassword, fullName, phoneNo, profilepic.filename]);
+    console.log("User Created with UID: ", uid)
+    res.status(200).send('User created successfully');
   } catch (error) {
-      console.error('Error creating user:', error);
-      res.status(500).send('Internal Server Error');
+    console.error('Error creating user:', error);
+    res.status(500).send('Internal Server Error');
   }
 });
 
 app.post('/login', async (req, res) => {
   try {
     const { email, password } = req.body;
-    if(!email || !password){
-      res.status(404).send({ValidUser: false, Status: "Fill All The Required Fields" });
-      return; 
+    if (!email || !password) {
+      res.status(404).send({ ValidUser: false, Status: "Fill All The Required Fields" });
+      return;
     }
     const query = `SELECT * FROM Users WHERE email = $1`;
     const result = await pool.query(query, [email]);
@@ -67,8 +67,8 @@ app.post('/login', async (req, res) => {
         };
         const token = generateToken(payload);
 
-        res.status(200).send({ user, token,ValidUser: true, Status: "User Logged-In" });
- 
+        res.status(200).send({ user, token, ValidUser: true, Status: "User Logged-In" });
+
       } else {
         res.status(404).send({ ValidUser: false, Status: "Invalid Credentials" });
       }
@@ -81,7 +81,27 @@ app.post('/login', async (req, res) => {
   }
 });
 
+app.post('/AddUserChat', async (req, res) => {
 
+  try {
+    const { chat_guid, sender_guid, receiver_guid } = req.body
+    const query = `Insert into UserChats (chat_guid, sender_guid, receiver_guid) VALUES ($1, $2, $3)`
+    const response = await pool.query(query, [chat_guid, sender_guid, receiver_guid])
+    const senderQuery = ` SELECT fullname FROM users WHERE uid = $1`;
+    const senderResult = await pool.query(senderQuery, [sender_guid]);
+    const sender_fullname = senderResult.rows[0].fullname;
+
+    const receiverQuery = `SELECT fullname FROM users WHERE uid = $1`;
+    const receiverResult = await pool.query(receiverQuery, [receiver_guid]);
+    const receiver_fullname = receiverResult.rows[0].fullname;
+    console.log(`New UserChat Created beteen ${sender_fullname} and ${receiver_fullname}`)
+    res.status(200).send(`New UserChat Created beteen ${sender_fullname} and ${receiver_fullname}`);
+
+  } catch (error) {
+    console.error('Error creating New UserChat:', error);
+    res.status(500).send('Internal Server Error');
+  }
+})
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
